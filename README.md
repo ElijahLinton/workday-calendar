c798b64c9ace42c5dc63da0086fad8f16bfcd214
karasu yami <demigodlinty@gmail.com>
2022-10-16 19:43:37 -0500

    colors change depending on time block added on load function all code commented with an updated readme

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # workday-calendar
+
+## Description
+Have you ever need a place where you can store task and notes on the internet? something you can have forever? the workday calendar will do just that. this is the webpage you can go to to save it all. with three different colors to alert the user what time is it out of the day its features will surely be comfortable for the user.it is deployed and ready to go i hope you enjoy!
+
+
+
+- [Installation](#Installion)
+- [Usage](#Usage.)
+- [Credits](#project)
+- [License](#Liscense)
+
+## Installation
+
+N/A
+
+## Usage
+when you come on to the web page you have to understand everything about it to use it properly. the numbers in sequential order are the hours in Military clock format.
+you can type anything you wish just make sure you click th blue button that says "save" to make sure its solidified to stay on your webpage whenever you come back to it. the textbox colors will vary depending on what time it is there are three colors red meaning present grey meaning past and green meaning future. you can save text right now before and in the future when ever you want.  
+## Screenshot
+
+
+## Link
+
+## Credits
+
+N/A
+
+## License
+
+MIT License
+
+Copyright (c) 2022 ElijahLinton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,83 @@
-    
+    //created variables and assigned them to the save buttons 
     var clickButton = document.getElementsByClassName('saveBtn')
+    //set the time in military clock format on the header to make it easier for the user 
+    //to follow along
     var timeDate = moment();
 $("#currentDay").text(timeDate.format("MMMM Do YYYY, HH:mm a"))
 
 
+
     
-$(".saveBtn").on("click",storeInfo) 
+//function used to to take text input values and store them within the local storage
 function storeInfo () {
-    
+    // text area grabs the value of specific class value 
     const textArea = $(this).siblings(".textInput").val();
     
 
-
+// dyatime blockis assigned to the name of the id
     const dayTimeBlock = $(this).parent().attr('id');
 
 
 
     
-
+//stores the text area value and the name of the id  in the local storage
     localStorage.setItem(dayTimeBlock, textArea);
+
+
 }
 
-    window.onload = function() {
-var notesValue = localStorage.getItem("notes",);
-    if (notesValue !== null){ $('.textInput').val(notesValue)}
-}
+//clock is a real time function that changes colors depending on what the time it is 
+ function clock(){
+    var currentTime = moment().hour();
     
- 
+    $('.row').each(function() {
+        var timeSelection = parseInt($(this).attr('id').split("militarytime")[1]);
+        if (timeSelection < currentTime){
+            $(this).removeClass('present')
+            $(this).removeClass('future')
+            $(this).addClass('past')
+        }
+        else if (timeSelection === currentTime){
+            $(this).removeClass('future')
+            $(this).removeClass('past')
+            $(this).addClass('present')
+        }
+        else{
+            $(this).removeClass('present')
+            $(this).removeClass('past')
+            $(this).addClass('future')
+        }
+    })
+ }
+
+// event listener  that calls a function every time a button is press
+$(".saveBtn").on("click",storeInfo) 
+ //loads recent local storage data on the text area 
+window.onload = function() {
+    $("#militarytime0 .textInput").val(localStorage.getItem('hour0'))
+    $("#militarytime1 .textInput").val(localStorage.getItem('hour1'))
+    $("#militarytime2 .textInput").val(localStorage.getItem('hour2'))
+    $("#militarytime3 .textInput").val(localStorage.getItem('hour3'))
+    $("#militarytime4 .textInput").val(localStorage.getItem('hour4'))
+    $("#militarytime5 .textInput").val(localStorage.getItem('hour5'))
+    $("#militarytime6 .textInput").val(localStorage.getItem('hour6'))
+    $("#militarytime7 .textInput").val(localStorage.getItem('hour7'))
+    $("#militarytime8 .textInput").val(localStorage.getItem('hour8'))
+    $("#militarytime9 .textInput").val(localStorage.getItem('hour9'))
+    $("#militarytime10 .textInput").val(localStorage.getItem('hour10'))
+    $("#militarytime11 .textInput").val(localStorage.getItem('hour11'))
+    $("#militarytime12 .textInput").val(localStorage.getItem('hour12'))
+    $("#militarytime13 .textInput").val(localStorage.getItem('hour13'))
+    $("#militarytime14 .textInput").val(localStorage.getItem('hour14'))
+    $("#militarytime15.textInput").val(localStorage.getItem('hour15'))
+    $("#militarytime16 .textInput").val(localStorage.getItem('hour16'))
+    $("#militarytime17 .textInput").val(localStorage.getItem('hour17'))
+    $("#militarytime18 .textInput").val(localStorage.getItem('hour18'))
+    $("#militarytime19 .textInput").val(localStorage.getItem('hour19'))
+    $("#militarytime20 .textInput").val(localStorage.getItem('hour20'))
+    $("#militarytime21 .textInput").val(localStorage.getItem('hour21'))
+    $("#militarytime22 .textInput").val(localStorage.getItem('hour22'))
+    $("#militarytime23 .textInput").val(localStorage.getItem('hour23'))
+    $("#militarytime24 .textInput").val(localStorage.getItem('hour24'))
+    clock();
+ }
